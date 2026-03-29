fa6c4fef3b36671d795c7abc811f403403220b3c
wenderson77 <wendersonpina10@gmail.com>
2026-03-29 15:21:15 -0300

    tornei os cards dinamicos

---
+++ controller/main.js
@@ -0,0 +1,5 @@
+import { renderProducts } from "./productsController.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderProducts();
+});
---
+++ model/products.js
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    title: "Pão de Leite Artesanal",
+    price: "R$ 0,89",
+    image: "assets/paes_background.jpg",
+    description: "Descubra a diferença de um pão assado no calor de um autêntico forno a lenha...",
+    benefits: [
+      "Farinha Selecionada das Melhores Fazendas",
+      "Leite Integral Fresco da Região",
+      "Fermentação Natural de Lenta Maturação",
+      "Tradição e Cuidado em Cada Peça"
+    ]
+  },
+
+  {
+    title: "Pão Integral Caseiro",
+    price: "R$ 1,20",
+    image: "assets/paes_segundo-background.png",
+    description: "Uma opção saudável e deliciosa, feita com grãos selecionados...",
+    benefits: [
+      "Rico em fibras",
+      "Sem conservantes",
+      "Fermentação natural",
+      "Sabor autêntico"
+    ]
+  }
+];
